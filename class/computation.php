7940a93f0303b1a2e56cd786001de503e1c19d17
matt <skinnerfam95@yahoo.com>
2013-08-02 13:43:21 -0600

    updated test

--- class/computation.php
+++ class/computation.php
@@ -1,12 +1,53 @@
 <?php
 class Computation{
-	private $m_age = 0;
+	private $m_numList = array();
 	
-	public function getAge(){
-		return $this->m_age;
+	public function __construct($numList){
+		if(is_array($numList)){
+			$this->m_numList = $numList;
+		}else{
+			throw new Exception('Array required');
+		}
+		
+		foreach($this->m_numList as $num){
+			if(!is_numeric($num)){
+				throw new Exception('Non numeric input');
+			}
+		}
 	}
 	
-	public function addOneToAge(){
-		$this->m_age = $this->m_age + 1;
+	public function sum(){
+		$rtn = 0;
+		foreach($this->m_numList as $intVal){
+			$rtn += $intVal;
+		}
+		
+		return $rtn;
+	}
+	
+	public function median(){
+		$rtn = 0;
+		$sorted = $this->m_numList;
+		
+		if(!sort($this->m_numList)){
+			throw new Exception('Sort error');
+		}
+		
+		$listLen = count($this->m_numList);
+		
+		if($listLen === 0){
+			throw new Exception('Median calc requires list length > 0');
+			
+		}elseif($listLen === 1){
+			$rtn = $sorted[0];
+			
+		}elseif($listLen % 2 === 0){
+			$rtn = ($sorted[$listLen/2] + $sorted[($listLen/2) + 1]) / 2;
+			
+		}else{
+			$rtn = $sorted[ floor($listLen/2) ];
+		}
+				
+		return $rtn;
 	}
 }
--- test/ComputationTest.php
+++ test/ComputationTest.php
@@ -3,14 +3,42 @@ require_once('../class/computation.php');
 
 class ComputationTest extends PHPUnit_Framework_TestCase
 {
-    public function testAddOne()
+    public function testSumAllPositive()
     {
-    	$computation = new Computation();
+    	$computation = new Computation(array(1,2,3));
     	
-    	$ageBefore = $computation->getAge();
-    	$computation->addOneToAge();
-    	$ageAfter = $computation->getAge();
-    	    	
-        $this->assertEquals($ageBefore + 1, $ageAfter);
+    	$sum = $computation->sum();
+        $this->assertEquals(6, $sum);
+
+    	$computation = new Computation(array(3.3, 2.2, 3));
+    	
+    	$sum = $computation->sum();
+        $this->assertEquals(8.5, $sum);
+    }
+    
+    public function testNonNumericInput(){
+    	$this->setExpectedException('Exception');
+    	
+    	$computation = new Computation(array(1, 'a', 3));
+    }
+    
+    public function testMedianArrayLengthOne(){
+    	$computation = new Computation(array(1));
+    	
+    	$median = $computation->median();
+    	$this->assertEquals(1, $median);
+    }
+    
+    public function testMedianArrayLengthZero(){
+    	$this->setExpectedException('Exception');
+    	
+    	$computation = new Computation(array());
+    	$median = $computation->median();
+    }
+    
+    public function testNonArrayConstructor(){
+    	$this->setExpectedException('Exception');
+    	
+    	$computation = new Computation(null);
     }
 }
